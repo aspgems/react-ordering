0cd0ab0d2adfa112d012b84245c0bfbae1f700dd
eLafo <javierlafora@gmail.com>
2018-05-24 14:55:23 +0200

    changes API host for products fetchin

--- src/components/Catalogue/config.js
+++ src/components/Catalogue/config.js
@@ -1,3 +1,4 @@
-export const API_HOST = '';
-export const DEFAULT_QUERY = '/data/products.json';
+export const API_HOST = 'https://raw.githubusercontent.com';
+export const DEFAULT_QUERY =
+  '/aspgems/react-ordering/master/public/data/products.json';
 export const NAMESPACE = 'catalogue';
