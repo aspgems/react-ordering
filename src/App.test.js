40fb21d54cc4c65aca823836c0e2dbddfbd31eaf
Juan Salvador Pérez García <jsperezg@gmail.com>
2018-05-24 10:22:12 +0200

    fetch request stubbed in tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const fetchPromise = Promise.resolve({
+  json: () =>
+    Promise.resolve({
+      id: 'A101',
+      description: 'Screwdriver',
+      category: '1',
+      price: '9.75'
+    })
+});
+global.fetch = () => fetchPromise;
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
