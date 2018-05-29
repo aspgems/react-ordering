d8ded9bcca8e3438225efd11da4a1242bf070fe5
eLafo <javierlafora@gmail.com>
2018-05-29 16:21:09 +0200

    adds configuration by environment

--- src/config.js
+++ src/config.js
@@ -1,20 +1,24 @@
+console.log(process.env);
 export default {
   order: {
     api: {
-      host: 'http://localhost:3001',
+      host: process.env.REACT_APP_ORDERS_API_HOST,
       orderPath: id => {
         if (id === null || id === undefined) {
-          return '/orders';
+          return process.env.REACT_APP_ORDERS_API_PATH;
+        } else if (process.env.NODE_ENV === 'production') {
+          // This is a hack which should be removed when API is deployed
+          return `${process.env.REACT_APP_ORDERS_API_PATH}/${id}.json`;
+        } else {
+          return `${process.env.REACT_APP_ORDERS_API_PATH}/${id}`;
         }
-
-        return '/orders/' + id;
       }
     }
   },
   products: {
     api: {
-      host: 'http://localhost:3001',
-      productsPath: '/products'
+      host: process.env.REACT_APP_PRODUCTS_API_HOST,
+      productsPath: process.env.REACT_APP_PRODUCTS_API_PATH
     }
   }
 };
